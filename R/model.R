e0f4c2612bd0c7fd7eb63dd8059df8527e7e1298
Charline <charline@saagie.com>
2017-02-28 16:16:07 +0100

    Adapt code with the platform 1.2 and add roxygen

--- R/model.R
+++ R/model.R
@@ -1,8 +1,17 @@
 # Read a file where there are the different Platform name
+#' Title
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.readTablePlatform <- function(path){
   file <- file.path(path, "platform", "platform.csv")
   if (file.exists(file)) {
-    res <- read.csv(file = file, sep=",", stringsAsFactors = FALSE)
+      res <- read.csv(file = file, sep=",", stringsAsFactors = FALSE)
+      res <- validator.filePlatformConform(res,file)
   } else {
     res <- data.frame("user" = character(0), "platformName" = character(0),
                       "password" = character(0), "platformURL" = character(0), "idPlatform" = integer(0), stringsAsFactors = FALSE)
@@ -12,10 +21,19 @@ model.readTablePlatform <- function(path){
 }
 
 # Read a file containing job R
+#' Title
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.readTableJob <- function(path){
   file <- file.path(path, "platform", "job.csv")
   if (file.exists(file)) {
     res <- read.csv(file = file, sep=",", stringsAsFactors = FALSE)
+    res <- validator.fileTableJobConform(res,file)
   } else {
     res <- data.frame("idJob" = integer(0), "idPlatform" = character(0), "capsule" = character(0), "category" = character(0),
                       "numVersion" = integer(0), "nameScript" = character(0), "nameJob" = character(0), stringsAsFactors = FALSE)
@@ -25,10 +43,19 @@ model.readTableJob <- function(path){
 }
 
 # Read a file containing the Platform where the job is upload
+#' Title
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.readThePlatform <- function(path){
   file <- file.path(path, "platform", "thePlatform.csv")
   if (file.exists(file)) {
     res <- read.csv(file = file, sep=",", stringsAsFactors = FALSE)
+    res <- validator.filePlatformConform(res,file)
   } else {
     res <- data.frame("user" = character(0), "platformName" = character(0), 
                       "password" = character(0), "platformURL" = character(0), "idPlatform" = integer(0), stringsAsFactors = FALSE)
@@ -38,10 +65,19 @@ model.readThePlatform <- function(path){
 }
 
 # Read a file containing the different platform where the user is access
+#' Title
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.readNamePlatform <- function(path){
   file <- file.path(path, "platform", "namePlatform.csv")
   if (file.exists(file)) {
     res <- read.csv(file = file, sep=",", stringsAsFactors = FALSE)
+    res <- validator.fileNamesPlatformConform(res,file)
   } else {
     res <- data.frame("id" = integer(0), "Names" = character(0), stringsAsFactors = FALSE)
     write.csv(res, file = file, row.names = FALSE)
@@ -50,6 +86,14 @@ model.readNamePlatform <- function(path){
 }
 
 # Read a file containing the num where upgrade the job
+#' Title
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.readNumJob <- function(path){
   file <- file.path(path, "platform", "row.csv")
   if (file.exists(file)) {
@@ -88,14 +132,15 @@ model.updateTablePlatform <- function(path, input){
     }
   }
 }
-#' Add the Job R in file (Depends on the platform)
+
+#' Title
 #'
-#' @param path Path to local persistent Saagie folder
+#' @param path 
 #'
-#' @importFrom httr GET authenticate content
+#' @return
+#' @export
+#'
 #' @importFrom RCurl base64Decode
-#' @return We don't know yet.
-#'
 model.JobRPlatform <- function(path){
   dataPlatform <- model.readThePlatform(path)
   withProgress({
@@ -105,7 +150,10 @@ model.JobRPlatform <- function(path){
   # TODO: Replace "as = 'parsed'" since it is strongly discouraged in a package. See 'help(content)'
   job <- content(reponse,type="application/json")
   j=1
-  listJob <- model.readTableJob(path)
+  listJob <- data.frame("idJob" = integer(0), "idPlatform" = character(0), "capsule" = character(0), "category" = character(0),
+                    "numVersion" = integer(0), "nameScript" = character(0), "nameJob" = character(0), stringsAsFactors = FALSE)
+  write.csv(listJob, file = file.path(path, "platform", "job.csv"), row.names = FALSE)
+  # listJob <- model.readTableJob(path)
   for(i in seq_along(job)){
     if(job[[i]]["capsule_code"]=="r"){
       listJob[j, "idJob"] <- job[[i]]["id"]
@@ -119,6 +167,16 @@ model.JobRPlatform <- function(path){
 }
 
 # Post a Job in the platform
+#' Title
+#'
+#' @param path 
+#' @param input 
+#' @param pathNameFile 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.postJob <- function(path, input, pathNameFile){
   thePlatform <- model.readThePlatform(path)
   fileName <- model.uploadFile(thePlatform, pathNameFile)
@@ -128,6 +186,15 @@ model.postJob <- function(path, input, pathNameFile){
 }
 
 # Post a File
+#' Title
+#'
+#' @param thePlatform 
+#' @param pathNameFile 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.uploadFile <- function(thePlatform, pathNameFile){
   reponseFile <- POST(paste(thePlatform[1,4],"/api/v1/platform/",thePlatform[1,5],"/job/upload",sep=""),
                       authenticate(thePlatform[1,1],base64Decode(thePlatform[1,3]),type="basic"),
@@ -136,6 +203,16 @@ model.uploadFile <- function(thePlatform, pathNameFile){
   return(fileName)
 }
 # Post a Job
+#' Title
+#'
+#' @param input 
+#' @param thePlatform 
+#' @param fileName 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.uploadJob <- function(input,thePlatform,fileName){
   req_ <-  POST(paste(thePlatform[1,4],"/api/v1/platform/",thePlatform[1,5],"/job",sep=""),
                 authenticate(thePlatform[1,1],base64Decode(thePlatform[1,3]),type="basic"),
@@ -160,6 +237,14 @@ model.runJob <- function(thePlatform,reponseAdd){
   return(idJobPlatform)
 }
 
+#' Title
+#'
+#' @param reponseAdd 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.idJobPlatform <- function(reponseAdd){
   addDeployName <- content(reponseAdd,type="application/json")
   idJobPlatform <- addDeployName[[1]]
@@ -167,6 +252,14 @@ model.idJobPlatform <- function(reponseAdd){
 }
 
 # Empty the file who containing the Job
+#' Title
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.rmJob <- function(path){
   # data <- model.readTableJob(path = path)
   # # data <- read.csv(file= file.path(path, "platform", "job.csv"),sep=",", stringsAsFactors = FALSE)
@@ -181,6 +274,14 @@ model.rmJob <- function(path){
 }
 
 # Add the default platform
+#' Title
+#'
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.defaultPlatform <- function(path){
   platformRun <- model.readThePlatform(path)
   userGo <- platformRun[1,1]
@@ -189,6 +290,19 @@ model.defaultPlatform <- function(path){
 }
 
 # Add the platform in file "ThePlatform.csv"
+#' Title
+#'
+#' @param path 
+#' @param userGo 
+#' @param platformNameGo 
+#' @param mdp 
+#' @param adressPlatform 
+#' @param numPlatform 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.addThePlatformInFile <- function(path, userGo,platformNameGo,mdp,adressPlatform,numPlatform){
   platformRun <- model.readThePlatform(path = path)
   platformRun[1,1] <- userGo
@@ -201,6 +315,15 @@ model.addThePlatformInFile <- function(path, userGo,platformNameGo,mdp,adressPla
 }
 
 # Add the select platform
+#' Title
+#'
+#' @param path 
+#' @param nb_row 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.selectPlatform <- function(path, nb_row){
   readRow <- model.readTablePlatform(path)
   userGo <- readRow[nb_row,1]
@@ -212,6 +335,15 @@ model.selectPlatform <- function(path, nb_row){
 }
 
 # Empty the file who containing the names platform
+#' Title
+#'
+#' @param path 
+#' @param namePlatform 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.rmNamePlatform <- function(path, namePlatform){
   if(nrow(namePlatform)!=0){
     for(i in 0:nrow(namePlatform)){
@@ -222,6 +354,15 @@ model.rmNamePlatform <- function(path, namePlatform){
 }
 
 # Add the different name platform where the user is access (in file namePlatform.csv)
+#' Title
+#'
+#' @param path 
+#' @param repPlatform 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.addNamePlatform <- function(path, repPlatform) {
   # namePlatform <- read.csv(file = file.path(path, "platform", "namePlatform.csv"), sep=",", stringsAsFactors = FALSE)
   namePlatform <- model.readNamePlatform(path = path)
@@ -235,6 +376,15 @@ model.addNamePlatform <- function(path, repPlatform) {
 }
 
 # Recover the name Platform when the success connection
+#' Title
+#'
+#' @param path 
+#' @param input 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.recoverNamePlatform <- function(path, input){
   platform <- GET(paste(input$platformURL,"/api/v1/platform", sep=""),
                   authenticate(input$user,input$password,type="basic"))
@@ -245,6 +395,17 @@ model.recoverNamePlatform <- function(path, input){
 }
 
 # Upgrade a job in the platform Saagie
+#' Title
+#'
+#' @param path_to_persistent_saagie_files 
+#' @param input 
+#' @param idJob 
+#' @param pathNameFile 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.upgradeJob <- function(path_to_persistent_saagie_files, input,idJob,pathNameFile){
   thePlatform <- model.readThePlatform(path_to_persistent_saagie_files)
   model.infoJobUpgrade(thePlatform,idJob)
@@ -258,6 +419,17 @@ model.upgradeJob <- function(path_to_persistent_saagie_files, input,idJob,pathNa
 }
 
 # Post a new version (upgrade)
+#' Title
+#'
+#' @param input 
+#' @param thePlatform 
+#' @param idJob 
+#' @param fileName 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.newVersion <- function(input,thePlatform,idJob,fileName){
   req_ <- POST(paste(thePlatform[1,4],"/api/v1/platform/",thePlatform[1,5],"/job/",idJob,"/version", sep=""),
                authenticate(thePlatform[1,1],base64Decode(thePlatform[1,3]),type="basic"),
@@ -268,6 +440,15 @@ model.newVersion <- function(input,thePlatform,idJob,fileName){
 }
 
 # Informations about the job upgrade
+#' Title
+#'
+#' @param thePlatform 
+#' @param idJob 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.infoJobUpgrade <- function(thePlatform,idJob){
   reponse <- GET(paste(thePlatform[1,4],"/api/v1/platform/",thePlatform[1,5],"/job/",idJob,sep=""),
                  authenticate(thePlatform[1,1],base64Decode(thePlatform[1,3]),type="basic"))
@@ -284,11 +465,23 @@ model.postUpgrade <- function(thePlatform,idJob,fileName){
 }
 
 # Return a current version, name job and name script of job R
+#' Title
+#'
+#' @param path 
+#' @param jobs 
+#' @param thePlatform 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.currentVersion <- function(path,jobs,thePlatform){
-  withProgress({
+  #withProgress({
     for (i in seq_len(nrow(jobs))) {
-      response <- GET(paste0(thePlatform[1, 4], "/api/v1/platform/", thePlatform[1, 5], "/job/", jobs[i,1]),
-                      authenticate(thePlatform[1, 1], base64Decode(thePlatform[1, 3]), type = "basic"))
+      response <- GET(paste(thePlatform[1,4], "/api/v1/platform/", thePlatform[1,5], "/job/", jobs[i,1],sep=""),
+                      authenticate(thePlatform[1,1], base64Decode(thePlatform[1,3]), type = "basic"))
+      # response <- GET(paste(thePlatform[1,4], "/api/v1/platform/", thePlatform[1,5], "/job/", jobs[3,1],sep=""),
+      #                 authenticate("service", "zP2khQwD3FChh", type = "basic"))
       detailsJob <- content(response, type = "application/json")
       # jobs[i, 5] <- detailsJob[[3]][[3]]
       jobs[i, "numVersion"] <- detailsJob[["current"]][["number"]]
@@ -297,7 +490,7 @@ model.currentVersion <- function(path,jobs,thePlatform){
       # jobs[i, 7] <- detailsJob[[7]]
       jobs[i, "nameJob"] <- detailsJob$name
     }
-  }, message = view.messageBarProgress())
+  #}, message = view.messageBarProgress())
   jobs <- jobs[order(jobs[,1],decreasing = T),]
   write.csv(jobs,file = file.path(path, "platform", "job.csv"), row.names = FALSE)
   #jobs <- jobs[, -1:-3]
@@ -306,6 +499,15 @@ model.currentVersion <- function(path,jobs,thePlatform){
 }
 
 # Recover a log
+#' Title
+#'
+#' @param thePlatform 
+#' @param idJob 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.showLog <- function(thePlatform,idJob){
   reponseLog <- GET(paste(thePlatform[1,4],"/api/v1/platform/",thePlatform[1,5],"/job/",idJob, "/jobinstance", sep=""),
                     authenticate(thePlatform[1,1],base64Decode(thePlatform[1,3]),type="basic"))
@@ -318,6 +520,15 @@ model.showLog <- function(thePlatform,idJob){
 }
 
 # Recover a log "stdout" (when the job is Run) in file "stdout.csv"
+#' Title
+#'
+#' @param thePlatform 
+#' @param test 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.downloadStdout <- function(thePlatform,test){
   data <- GET(paste(thePlatform[1,4],"/api/v1/jobinstance/",test[['IdLog']],"/stdout", sep=""),
               authenticate(thePlatform[1,1],base64Decode(thePlatform[1,3]),type="basic"))
@@ -328,6 +539,15 @@ model.downloadStdout <- function(thePlatform,test){
 }
 
 # Recover a log "stderr" (when the job is Run) in file "stderr.csv"
+#' Title
+#'
+#' @param thePlatform 
+#' @param test 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.downloadStderr <- function(thePlatform, test){
   data <- GET(paste(thePlatform[1,4],"/api/v1/jobinstance/",test[['IdLog']],"/stderr", sep=""),
               authenticate(thePlatform[1,1],base64Decode(thePlatform[1,3]),type="basic"))
@@ -338,6 +558,15 @@ model.downloadStderr <- function(thePlatform, test){
 }
 
 # Write a file who upload in a platform
+#' Title
+#'
+#' @param document 
+#' @param nameFile 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.writeFile <- function(document, nameFile) {
   #pathNameFile <- paste("platform/", nameFile, ".R", sep="")
   pathNameFile <- tempfile()
@@ -346,17 +575,27 @@ model.writeFile <- function(document, nameFile) {
 }
 
 # Remove a file who upload in a platform
+#' Title
+#'
+#' @param pathNameFile 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model.removeFile <- function(pathNameFile){
   file.remove(pathNameFile)
 }
-#' Remove no script R and displays the name of script R
+
+#' Title
 #'
-#' @param jobs We don't know yet.
+#' @param jobs 
 #'
-#' @return We don't know yet.
+#' @return
+#' @export
+#'
 #' @importFrom data.table tstrsplit
 #' @importFrom stringr str_sub
-#'
 model.removeLinkedNoR <- function(jobs){
   for(i in 1:nrow(jobs)){
     if((str_sub(jobs[i,"nameScript"],-2, -1) == ".R")){
--- R/validator.R
+++ R/validator.R
@@ -1,4 +1,13 @@
 # Check if all fields (containing the information of the platform) are OK
+#' Title
+#'
+#' @param path 
+#' @param input 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 validator.infoPlatform <- function(path, input){
   if(is.null(input$platformURL) || input$platformURL == "" ||
      is.null(input$user) || input$user == "" ||
@@ -10,6 +19,15 @@ validator.infoPlatform <- function(path, input){
 }
 
 # Check the connection to the platform
+#' Title
+#'
+#' @param path 
+#' @param input 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 validator.testConnection <- function(path, input){
   # repCon <- GET(paste(input$platformURL,"/login",sep=""),authenticate(input$user,input$password,type="basic"))
   repCon <- model.recoverNamePlatform(path, input)
@@ -43,6 +61,14 @@ validator.infoJob <- function(input){
   })
 }
 
+#' Title
+#'
+#' @param input 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 validator.mail <- function(input){
   if(input$createEmail == "" || is.null(input$createEmail)){
     shinyjs::enable("addDeploy")
@@ -63,4 +89,85 @@ validator.mail <- function(input){
       shinyjs::disable("addDeploy")
     }
   }
+}
+
+#' Title
+#'
+#' @param file 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+validator.filePlatformConform <- function(res,file){
+  if(length(names(res)) == 5){
+    if(!((names(res[1]) == "user") && (names(res[2]) == "platformName") && (names(res[3]) == "password") && (names(res[4]) == "platformURL") && (names(res[5]) == "idPlatform"))){
+      res <- data.frame("user" = character(0), "platformName" = character(0),
+                        "password" = character(0), "platformURL" = character(0), "idPlatform" = integer(0), stringsAsFactors = FALSE)
+      write.csv(res, file = file, row.names = FALSE)
+    }
+  }else{
+    res <- data.frame("user" = character(0), "platformName" = character(0),
+                      "password" = character(0), "platformURL" = character(0), "idPlatform" = integer(0), stringsAsFactors = FALSE)
+    write.csv(res, file = file, row.names = FALSE)
+  }
+  res
+}
+
+#' Title
+#'
+#' @param res 
+#' @param file 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+validator.fileNamesPlatformConform <- function(res,file){
+  if(length(names(res)) == 2){
+    if(!((names(res[1]) == "id") && (names(res[2]) == "Names"))){
+      res <- data.frame("id" = integer(0), "Names" = character(0), stringsAsFactors = FALSE)
+      write.csv(res, file = file, row.names = FALSE)
+    }
+  }else{
+    res <- data.frame("id" = integer(0), "Names" = character(0), stringsAsFactors = FALSE)
+    write.csv(res, file = file, row.names = FALSE)
+  }
+  res
+}
+
+#' Title
+#'
+#' @param res 
+#' @param file 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+validator.fileTableJobConform <- function(res,file){
+  if(length(names(res)) == 7){
+    if(!(names(res[1]) == "idJob" && (names(res[2]) == "idPlatform") && (names(res[3]) == "capsule") && (names(res[4]) == "category")
+         && (names(res[5]) == "numVersion") && (names(res[6]) == "nameScript") && (names(res[7]) == "nameJob"))){
+      res <- data.frame("idJob" = integer(0), "idPlatform" = character(0), "capsule" = character(0), "category" = character(0),
+                        "numVersion" = integer(0), "nameScript" = character(0), "nameJob" = character(0), stringsAsFactors = FALSE)
+      write.csv(res, file = file, row.names = FALSE)
+    }
+  }else{
+    res <- data.frame("idJob" = integer(0), "idPlatform" = character(0), "capsule" = character(0), "category" = character(0),
+                      "numVersion" = integer(0), "nameScript" = character(0), "nameJob" = character(0), stringsAsFactors = FALSE)
+    write.csv(res, file = file, row.names = FALSE)
+  }
+  res
+  # if(length(names(res)) == 4){
+  #   if(!(names(res[1]) == "idJob" && (names(res[2]) == "idPlatform") && (names(res[3]) == "capsule") && (names(res[4]) == "category"))){
+  #     res <- data.frame("idJob" = integer(0), "idPlatform" = character(0), "capsule" = character(0), "category" = character(0),stringsAsFactors = FALSE)
+  #     write.csv(res, file = file, row.names = FALSE)
+  #   }
+  # }else{
+  #   res <- data.frame("idJob" = integer(0), "idPlatform" = character(0), "capsule" = character(0), "category" = character(0),
+  #                     "numVersion" = integer(0), "nameScript" = character(0), "nameJob" = character(0), stringsAsFactors = FALSE)
+  #   write.csv(res, file = file, row.names = FALSE)
+  # }
+  # res
 }
